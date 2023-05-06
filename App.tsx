be4b6f65f738bc3cf6cc47e7b00953e31c3b5519
carlos <carlosa@xgrow.com>
2023-05-06 18:32:43 -0300

    Feat: Create Status Bar

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { Loading } from "@components/Loading";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import { Groups } from "@screens/Groups";
 import theme from "@theme/index";
+import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components";
 
 export default function App() {
@@ -11,7 +12,12 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      { !fontsLoaded ? <Groups /> : <Loading /> }
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      { fontsLoaded ? <Groups /> : <Loading /> }
     </ThemeProvider>
   );
 }
