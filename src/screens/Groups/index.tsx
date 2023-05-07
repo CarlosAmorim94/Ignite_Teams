2155c5e46f1660637b8211d3117767a39c29aafc
carlos <carlosa@xgrow.com>
2023-05-07 16:25:58 -0300

    Feat: Create Button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,6 +4,8 @@ import { Highlight } from "@components/HighLigth";
 import { GroupCard } from "@components/GroupCard";
 import { useState } from "react";
 import { FlatList } from "react-native";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Groups() {
 
@@ -19,9 +21,14 @@ export function Groups() {
         data={groups}
         keyExtractor={item => item}
         renderItem={({item}) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && {flex: 1}}
+        ListEmptyComponent={() => <ListEmpty message="Que tal cadastrar a primeira turma?" />}
       />
 
-
+    <Button
+      title="Cadastrar turma"
+      onPress={() => {}}
+    />
     </Container>
   );
 }
