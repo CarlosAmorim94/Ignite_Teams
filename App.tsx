af37c0919150c2b4fe01136a18af91b99c2da90b
carlos <carlosa@xgrow.com>
2023-05-07 16:38:37 -0300

    Feat: Create components

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { Loading } from "@components/Loading";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import { Groups } from "@screens/Groups";
+import { NewGroup } from "@screens/NewGroup";
 import theme from "@theme/index";
 import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components";
@@ -17,7 +18,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <Groups /> : <Loading /> }
+      { fontsLoaded ? <NewGroup /> : <Loading /> }
     </ThemeProvider>
   );
 }
---
+++ src/screens/NewGroup/index.tsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+import { Button } from "@components/Button";
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { Input } from "@components/Input";
+import { useNavigation } from "@react-navigation/native";
+// import { createGroup } from "@storage/group/createGroup";
+// import { AppError } from "@utils/AppError";
+// import { Alert } from "react-native";
+import { Container, Content, Icon } from "./styles";
+
+export function NewGroup() {
+  const [group, setGroup] = useState("");
+
+  const navigation = useNavigation();
+
+  async function handleNewGroup() {
+    // try {
+    //   if (group.trim().length === 0) {
+    //     return Alert.alert("Novo grupo", "Informe o nome da turma");
+    //   }
+
+    //   await createGroup(group);
+    //   navigation.navigate("players", { group });
+
+    //   setGroup("");
+    // } catch (error) {
+    //   if (error instanceof AppError) {
+    //     return Alert.alert("Novo grupo", error.message);
+    //   } else {
+    //     return Alert.alert(
+    //       "Novo grupo",
+    //       "Não foi possível criar um novo grupo"
+    //     );
+    //   }
+    // }
+  }
+
+  return (
+    <Container>
+      <Header showBackButton />
+      <Content>
+        <Icon />
+        <Highlight
+          title="Nova turma"
+          subTitle="crie uma turma para adicionar pessoas"
+        />
+        <Input
+          placeholder="Nome da turma"
+          value={group}
+          onChangeText={setGroup}
+        />
+        <Button
+          title="Criar"
+          style={{ marginTop: 20 }}
+          onPress={handleNewGroup}
+        />
+      </Content>
+    </Container>
+  );
+}
